8ed01e9983a0078fc7cd0aae4cfa3efdf46b1ed0
nancy <nancy@DESKTOP-V20DE6F>
2023-02-26 22:27:57 -0500

    Add project files.

---
+++ Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        [HttpGet]
+
+        public string Menu(int burger, int drink, int side, int dessert)
+        //J1 Problem - (adapted) The New CCC
+        ///<summary>
+        /// This function calculates the total calories of a meal based on the food items selected from each category - burger, drink, side, dessert.
+        ///</summary>
+        ///<returns>
+        /// "Your calorie count is" + the total amount of calories
+        ///</returns>
+        ///<param name="burger"> choice of 1,2,3, or 4 -> each has different calorie count (burgercal) </param>
+        ///<param name="drink"> choice of 1,2,3, or 4 -> each has different calorie count (drinkcal) </param>
+        ///<param name="side"> choice of 1,2,3, or 4 -> each has different calorie count (sidecal)</param>
+        ///<param name="dessert"> choice of 1,2,3, or 4 -> each has different calorie count (dessertcal)</param>
+        ///<example>
+        ///GET localhost/api/J1/Menu/4/4/4/4 -> "Your total calorie count is 0"
+        ///GET localhost/api/J1/Menu/1/2/3/4 -> "Your total calorie count is 691"
+        ///</example>
+
+        {
+            int burgercal;
+            int drinkcal;
+            int sidecal;
+            int dessertcal;
+
+            if(burger == 1)
+            {
+                burgercal = 461;
+
+            } else if(burger == 2)
+            {
+                burgercal = 431;
+
+            } else if(burger == 3)
+            {
+                burgercal = 420;
+
+            } else 
+            {
+                burgercal = 0;
+            }
+
+            if(drink == 1)
+            {
+                drinkcal = 130;
+
+            } else if (drink == 2)
+            {
+                drinkcal = 160;
+
+            } else if (drink == 3)
+            {
+                drinkcal = 118;
+
+            } else
+            {
+                drinkcal = 0;
+            }
+
+            if(side == 1)
+            {
+                sidecal = 100;
+
+            } else if (side == 2)
+            {
+                sidecal = 57;
+
+            } else if (side == 3)
+            {
+                sidecal = 70;
+
+            } else
+            {
+                sidecal = 0;
+            }
+
+            if(dessert == 1)
+            {
+                dessertcal = 167;
+
+            } else if (dessert == 2)
+            {
+                dessertcal = 266;
+
+            } else if (dessert == 3)
+            {
+                dessertcal = 75;
+
+            } else
+            {
+                dessertcal = 0;
+            }
+
+            int totalcal = burgercal + drinkcal + sidecal + dessertcal;
+            string message = "Your total calorie count is ";
+            string output = message + totalcal;
+
+            return output;
+
+        }
+    }
+}
---
+++ Assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        [Route("api/J2/FishFinder/{a}/{b}/{c}/{d}")]
+        [HttpGet]
+
+        //J2 Problem - "Sounds Fishy!" from CCC 2012
+        ///<summary>
+        ///Function let's us know if a fish is found by using four depth readings
+        /// </summary>
+        /// <returns>
+        /// If the four consecutive depth readings form an increasing sequence -> "Fish Rising"
+        /// If the four consecutive depth readings form a decreasing sequence -> "Fish Diving"
+        /// If the four consecutive depth readings are identical -> "Fish At Constant Depth"
+        /// Any other sequence considered random noise or debris -> "No Fish"
+        /// </returns>
+        /// <param name="a"> first depth reading </param>
+        /// <param name="b"> second depth reading </param>
+        /// <param name="c"> third depth reading </param>
+        /// <param name="d"> fourth depth reading </param>
+        /// <example>
+        /// GET localhost/api/J2/FishFinder/30/10/20/20 -> "No Fish"
+        /// GET localhost/api/J2/FishFinder/1/10/12/13 -> "Fish Rising"
+        /// GET localhost/api/J2/FishFinder/3/3/3/3 -> "Fish At Constand Depth"
+        /// GET localhost/api/J2/FishFinder/10/9/8/7 -> "Fish Diving"
+        /// </example>
+        public string FishFinder(int a, int b, int c, int d)
+        {
+            if(a<b && b <c && c < d)
+            {
+                return "Fish Rising";
+
+            } else if (a>b && b>c && c > d)
+            {
+                return "Fish Diving";
+
+            } else if (a==b && b==c && c == d)
+            {
+                return "Fish At Constant Depth";
+
+            } else
+            {
+                return "No Fish";
+            }
+        }
+    }
+}
